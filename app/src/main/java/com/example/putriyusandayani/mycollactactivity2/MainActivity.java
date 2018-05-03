2b8063378aecee6d2e7faf1ab20e7ba7845812be
ePsari <evipermatanura872@gmail.com>
2018-05-03 10:12:40 +0800

    notification
    yang kerjkan berdua elis dan putri yus

--- app/src/main/java/com/example/putriyusandayani/mycollactactivity2/MainActivity.java
+++ app/src/main/java/com/example/putriyusandayani/mycollactactivity2/MainActivity.java
@@ -2,6 +2,14 @@ package com.example.putriyusandayani.mycollactactivity2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.SystemClock;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
 
 public class MainActivity extends AppCompatActivity {
 

69f6acc9e17267e4cb253d931a6b6830ad215570
Putriyusandayanii <putriyushandayani@gmail.com>
2018-05-19 10:57:16 +0700

    adapterjadwal
    atur jadwal

--- app/src/main/java/com/example/putriyusandayani/mycollactactivity2/AdapterJadwal.java
+++ app/src/main/java/com/example/putriyusandayani/mycollactactivity2/AdapterJadwal.java
@@ -13,7 +13,50 @@ import android.widget.TextView;
  */
 
 public class AdapterJadwal extends ArrayAdapter<Jadwal> {
+
     public AdapterJadwal(@NonNull Context context, int resource) {
         super(context, resource);
+    }
+    private Activity activity;
+    private ArrayList<Jadwal>lJadwal;
+    private static LayoutInflater inflater = null;
+    public AdapterJadwal (Activity activity, int textViewResourceId,ArrayList<Jadwal> al_Jadwal) {
+        super(activity, textViewResourceId, al_Jadwal);
+        try {
+            this.activity = activity;
+            this.lJadwal = al_Jadwal;
+            inflater = (LayoutInflater) activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        } catch (Exception e) {
+        }
+    }
+    public int getCount() {
+        return lJadwal.size();
+    }
+    public long getItemId(int position) {
+        return position;
+    }
+    public static class ViewHolder {
+        public TextView jam;
+        public TextView matkul;
+    }
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View vi = convertView;
+        final ViewHolder holder;
+        try {
+            if (convertView == null) {
+                vi = inflater.inflate(R.layout.row_jadwal, null);
+                holder = new ViewHolder();
+                holder.jam = (TextView) vi.findViewById(R.id.textViewRowJam);
+                holder.matkul = (TextView) vi.findViewById(R.id.textViewRowMataKuliah);
+                vi.setTag(holder);
+
+            } else {
+                holder = (ViewHolder) vi.getTag();
+            }
+            holder.jam.setText(lJadwal.get(position).jam);
+            holder.matkul.setText(lJadwal.get(position).matkul);
+        } catch (Exception e) {
+        }
+        return vi;
     }
 }
--- app/src/main/java/com/example/putriyusandayani/mycollactactivity2/MainActivity.java
+++ app/src/main/java/com/example/putriyusandayani/mycollactactivity2/MainActivity.java
@@ -13,6 +13,7 @@ import android.widget.RadioButton;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
